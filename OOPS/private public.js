18ce4ad5f3de9d549ab9bc968c7851dac5aabac4
RohithBoppey <rohithboppey1298@gmail.com>
2023-02-09 12:36:09 +0530

    modified private public

--- OOPS/private public.js
+++ OOPS/private public.js
@@ -1,19 +1,21 @@
 // we can use private by using return instead of normal
-function someClass(){
+var someClass = (function(){
     var contents = 0;
     function f(){
         contents++;
     }
 
+    // dealing with private like this
     return {
         increment: function(){
             f();
             console.log(`incremented, now: ${contents}`);
         }
     }
-}
+})();
 
-var v = someClass();
-v.increment()
-v.increment()
-console.log(v.contents)
+someClass.increment();
+someClass.increment();
+someClass.increment();
+someClass.increment();
+console.log(someClass.contents)
